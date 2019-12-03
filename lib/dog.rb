546161973c6cc6fd799120d0f472d379bbfa01f4
Nory Lazo <nlazo@icloud.com>
2019-12-03 04:26:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,7 +6,11 @@ class Dog
   
   def initialize(name)
     @name = name
-    @@all << self
+    
+    def save 
+      @@all << self
+    end 
+    
   end
   
   def self.all 
@@ -23,7 +27,5 @@ class Dog
     end
   end
   
-  def save 
-    @@all << self
-  end 
+  
 end 
